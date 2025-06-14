69a8c01f3481d7a294553a10c7aa9febf352d03e
krishn6772 <kumarkrishna3533@gmail.com>
2025-06-14 18:02:14 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,42 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import postRoure from "./routes/post.route.js";
+import messageRoute from "./routes/message.route.js";
+import { app, server } from "./socket/socket.js";
+import path from "path";
+
+dotenv.config({});
+
+const PORT = process.env.PORT || 3000;
+
+const __dirname = path.resolve();
+
+await connectDB();
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+const corsOptions = {
+    origin: process.env.PORT,
+    credentials: true
+}
+app.use(cors(corsOptions));
+
+// yha pr apni api ayengi
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/post", postRoure);
+app.use("/api/v1/message", messageRoute);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get('/*splat', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
