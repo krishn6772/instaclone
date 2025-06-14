4807345cf71e445566ee7b3aa53bf43e25897ded
krishn6772 <kumarkrishna3533@gmail.com>
2025-06-14 20:01:27 +0530

    host

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,7 @@ function App() {
 
   useEffect(() => {
     if (user) {
-      const socketio = io('https://instaclone-cy5z.onrender.com', {
+      const socketio = io('http://localhost:8000', {
         query: {
           userId: user?._id
         },
